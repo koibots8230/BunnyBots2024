1149ae66b85911a137387f9a525692bab3f53794
funneralplanner <plsdontatm3@gmail.com>
2024-11-07 16:25:31 -0500

    doesn't work, made back motors follow

--- src/main/java/com/koibots/robot/Subsystems/TankDrive.java
+++ src/main/java/com/koibots/robot/Subsystems/TankDrive.java
@@ -3,6 +3,10 @@ package com.koibots.robot.Subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.koibots.robot.Constants;
+
+import edu.wpi.first.cscore.VideoException;
+import edu.wpi.first.wpilibj.motorcontrol.Victor;
+import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class TankDrive extends SubsystemBase {
@@ -10,13 +14,19 @@ public class TankDrive extends SubsystemBase {
     public static TankDrive Tank_Drive = new TankDrive();
     private final CANSparkMax rightMainMotor;
     private final CANSparkMax leftMainMotor;
-
+    private final VictorSP leftBackMotor = new VictorSP(0);
+    private final VictorSP leftFrontMotor = new VictorSP(1);
+    private final VictorSP rightBackMotor = new VictorSP(0);
+    private final VictorSP rightFrontMotor = new VictorSP(1);
 
 
     public TankDrive(){
         rightMainMotor = new CANSparkMax(Constants.TankDriveConstants.RIGHT_MOTOR, MotorType.kBrushless);
         leftMainMotor = new CANSparkMax(Constants.TankDriveConstants.LEFT_MOTOR, MotorType.kBrushless);
-    //check to see if these motors need to be inverted!
+        
+        //check to see if these motors need to be inverted!
+
+    
     }
 
     public static TankDrive get() {
@@ -25,6 +35,7 @@ public class TankDrive extends SubsystemBase {
 
     public void setRightSpeed(double speed){
         rightMainMotor.set(speed);
+
     }
 
     public void setLeftSpeed(double speed){
@@ -33,6 +44,8 @@ public class TankDrive extends SubsystemBase {
 
     @Override
     public void periodic(){
+        leftBackMotor.setVoltage(leftMainMotor.getAppliedOutput()*leftMainMotor.getBusVoltage());
+        rightBackMotor.setVoltage(rightMainMotor.getAppliedOutput()*rightMainMotor.getBusVoltage());
     }
     
 }
